ab8573e00b9910c6de2986985a79867f273f32c7
anaalbsantos <6030.anaalbuquerque@gmail.com>
2024-11-28 10:01:46 -0300

    add routes and change Citi

--- server/src/controllers/ProductController.ts
+++ server/src/controllers/ProductController.ts
@@ -1,63 +1,60 @@
 import { Request, Response } from "express";
 import { Citi, Crud } from "../global";
 
-class ProductController{
-    constructor(private readonly citi = new Citi("Product")) {}
+class ProductController {
+  constructor(private readonly citi = new Citi("Product")) {}
 
-    create = async (request:Request, response:Response) => {
-        const { name, price, quantity } = request.body;
+  create = async (request: Request, response: Response) => {
+    const { name, price, quantity } = request.body;
 
-        const isAnyUndefined = this.citi.areValuesUndefined(
-            name,
-            price,
-            quantity
-        );
+    const isAnyUndefined = this.citi.areValuesUndefined(name, price, quantity);
 
-        if(isAnyUndefined) return response.status(400).send();
+    if (isAnyUndefined) return response.status(400).send();
 
-        const newProduct = { name, price, quantity };
-        const { httpStatus, message } = await this.citi.insertIntoDatabase(newProduct);
+    const newProduct = { name, price, quantity };
+    const { httpStatus, message } = await this.citi.insertIntoDatabase(
+      newProduct
+    );
 
-        return response.status(httpStatus).send({ message });
-    }
+    return response.status(httpStatus).send({ message });
+  };
 
-    get = async (request:Request, response:Response) => {
-        const { httpStatus, values } = await this.citi.getAll();
+  get = async (request: Request, response: Response) => {
+    const { httpStatus, values } = await this.citi.getAll();
 
-        return response.status(httpStatus).send(values);
-    }
+    return response.status(httpStatus).send(values);
+  };
 
-    getById = async (request:Request, response:Response) => {
-        const { id } = request.params;
+  getById = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    console.log(id);
 
-        const { httpStatus, value } = await this.citi.findById(id);
+    const { httpStatus, value } = await this.citi.findById(id);
+    console.log(value);
+    return response.status(httpStatus).send(value);
+  };
 
-        return response.status(httpStatus).send(value);
+  update = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { name, price, quantity } = request.body;
 
-    }
+    const updatedValues = { name, price, quantity };
 
-    update = async (request:Request, response:Response) => {
-        const { id } = request.params;
-        const { name, price, quantity } = request.body;
+    const { httpStatus, messageFromUpdate } = await this.citi.updateValue(
+      id,
+      updatedValues
+    );
 
-        const updatedValues = { name, price, quantity };
+    return response.status(httpStatus).send({ messageFromUpdate });
+  };
 
-        const { httpStatus, messageFromUpdate } = await this.citi.updateValue(
-            id,
-            updatedValues
-        );
+  delete = async (request: Request, response: Response) => {
+    const { id } = request.params;
 
-        return response.status(httpStatus).send({ messageFromUpdate });
-    }
+    const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
 
-    delete = async (request:Request, response:Response) => {
-        const { id } = request.params;
-
-        const { httpStatus, messageFromDelete } = await this.citi.deleteValue(id);
-
-        return response.status(httpStatus).send({ messageFromDelete });
-    }
-
+    return response.status(httpStatus).send({ messageFromDelete });
+  };
 }
 
 export default new ProductController();
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import ProductController from "./controllers/ProductController";
 
 const routes = express.Router();
 
@@ -7,5 +8,11 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+routes.post("/product", ProductController.create);
+routes.get("/product", ProductController.get);
+routes.get("/product/:id", ProductController.getById);
+routes.patch("/product/:id", ProductController.update);
+routes.delete("/product/:id", ProductController.delete);
 
 export default routes;
